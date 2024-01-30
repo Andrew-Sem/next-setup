6cafb1d91040252d5f43e7a00678073854172a66
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2024-01-30 23:27:11 +0300

    feat: change h1 at landing

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,7 +4,7 @@ import { CreateCourseForm } from "@/features/courses-list/pub/create-course-form
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8">
-      <h1 className="text-5xl mb-4">Courses</h1>
+      <h1 className="text-5xl mb-4">Next setup</h1>
       <CreateCourseForm revalidatePagePath="/" className="max-w-xs mb-10" />
       <CoursesList revalidatePagePath="/" />
     </main>
