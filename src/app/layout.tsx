d8893aaff9daca4815ae4950b53ec5ea872e449a
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2024-01-30 23:14:47 +0300

    refactor: refactor app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
-import type { Metadata } from "next";
 import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/shared/ui/utils";
-import { AppHeader } from "@/widgets/app-header/app-header";
-import { ThemeProvider } from "@/features/theme/theme-provider";
+import { AppProvider } from "./_providers/app-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -23,10 +21,7 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
-        <ThemeProvider>
-          <AppHeader />
-          {children}
-        </ThemeProvider>
+        <AppProvider>{children}</AppProvider>
       </body>
     </html>
   );
